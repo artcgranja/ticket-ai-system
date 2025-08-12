8dabb5b622f799f120d8a34186a50775eb8842e3
Arthur cavalcanti Granja <artcgranja@gmail.com>
2025-08-12 20:09:36 -0300

    [FEAT] Add BaseModel class to define common attributes for SQLAlchemy models, including id, created_at, and updated_at fields

--- app/models/base.py
+++ app/models/base.py
@@ -1,9 +1,16 @@
 from __future__ import annotations
 
+from sqlalchemy import Column, Integer, DateTime
 from sqlalchemy.orm import declarative_base
+from datetime import datetime
 
 
 # Shared SQLAlchemy Base for all models
 Base = declarative_base()
 
 
+class BaseModel(Base):
+    __abstract__ = True
+    id = Column(Integer, primary_key=True, index=True)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
